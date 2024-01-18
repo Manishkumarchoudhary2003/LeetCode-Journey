10743a49d7c226e5d9d8feff3b0014ce43ce5437
Manishkumarchoudhary2003 <cmanishkumar193@gmail.com>
2024-01-18 22:57:21 +0530

    18/01/2024

---
+++ removeDuplicates.java
@@ -0,0 +1,23 @@
+public class removeDuplicates {
+    class Solution {
+        public String removeDuplicates(String s) {
+    
+            Stack<Character> st = new Stack<>();
+            for(int i = 0; i < s.length(); i++){
+                char ch = s.charAt(i);
+                if(st.size() > 0 && ch == st.peek()){
+                    st.pop();
+                }else{
+                    st.push(ch);
+                }
+            }
+            StringBuffer ss = new StringBuffer();
+            while(st.size() > 0){
+                char c = st.pop();
+                ss.insert(0,c); 
+            }
+            return ss.toString(); 
+            
+        }
+    }
+}
