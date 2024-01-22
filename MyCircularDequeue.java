594091087a4b93967f679f486b669b22a7a27e93
Manishkumarchoudhary2003 <cmanishkumar193@gmail.com>
2024-01-22 10:50:45 +0530

    22/01/2024

---
+++ MyCircularDequeue.java
@@ -0,0 +1,85 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class MyCircularDequeue {
+    class MyCircularDeque {
+
+     static Deque<Integer> d;
+    static int size ;
+
+    public MyCircularDeque(int k) {
+         d =new LinkedList<>();
+         size = k;        
+    }
+    
+    public boolean insertFront(int value) {
+        if(size > d.size()){
+            d.addFirst(value);
+            return true;
+        }
+        return false;
+        
+    }
+    
+    public boolean insertLast(int value) {
+         if(size > d.size()){
+            d.addLast(value);
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean deleteFront() {
+        if(d.size() > 0){
+            d.removeFirst();
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean deleteLast() {
+         if(d.size() > 0){
+            d.removeLast();
+            return true;
+        }
+        return false;
+    }
+    
+    public int getFront() {
+        if(d.size() > 0){
+            return d.getFirst();
+        }
+        return -1;
+    }
+    
+    public int getRear() {
+         if(d.size() > 0){
+            return d.getLast();
+        }
+        return -1;
+    }
+    
+    public boolean isEmpty() {
+        if(d.size() == 0) return true;
+        else return false;
+    }
+    
+    public boolean isFull() {
+        if(d.size() == size) return true;
+        else return false;
+    }
+}
+
+/**
+ * Your MyCircularDeque object will be instantiated and called as such:
+ * MyCircularDeque obj = new MyCircularDeque(k);
+ * boolean param_1 = obj.insertFront(value);
+ * boolean param_2 = obj.insertLast(value);
+ * boolean param_3 = obj.deleteFront();
+ * boolean param_4 = obj.deleteLast();
+ * int param_5 = obj.getFront();
+ * int param_6 = obj.getRear();
+ * boolean param_7 = obj.isEmpty();
+ * boolean param_8 = obj.isFull();
+ */
+}
