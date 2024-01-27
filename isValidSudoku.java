f69c4f017578c4e301362e4960a5343fb6e68af6
Manishkumarchoudhary2003 <cmanishkumar193@gmail.com>
2024-01-27 15:08:37 +0530

    27/01/2024

---
+++ isValidSudoku.java
@@ -0,0 +1,32 @@
+public class isValidSudoku {
+    class Solution {
+        public boolean isSafe(char[][] board, int row, int col, char num){
+            for(int j = 0; j < 9; j++){
+                if(board[row][j] == num) return false;
+            }
+            for(int i = 0; i < 9; i++){
+                if(board[i][col] == num) return false;
+            }
+            int sr = row/3*3;
+            int sc = col/3*3;
+            for(int i = sr; i < sr+3; i++){
+                for(int j = sc; j < sc+3; j++){
+                    if(board[i][j] == num) return false;
+                }
+            }
+            return true;
+        }
+        public boolean isValidSudoku(char[][] board) {
+            for(int i =0 ; i< 9; i++){
+                for(int j =0; j < 9; j++){
+                    if(board[i][j] == '.') continue;
+                    char num = board[i][j];
+                    board[i][j] = '.'; 
+                    if(isSafe(board,i,j,num) == false) return false;
+                    board[i][j] = num;
+                }
+            }
+            return true;
+        }
+    }
+}
