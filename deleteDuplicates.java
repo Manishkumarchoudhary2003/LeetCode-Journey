183468335ba36ecd3ef1aa3b7febad07ccdb680c
Manishkumarchoudhary2003 <cmanishkumar193@gmail.com>
2024-01-29 23:31:01 +0530

    29/01/2024

---
+++ deleteDuplicates.java
@@ -0,0 +1,45 @@
+public class deleteDuplicates {
+
+    // * Definition for singly-linked list.
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    class Solution {
+        public ListNode deleteDuplicates(ListNode head) {
+            if (head == null || head.next == null) {
+                return head;
+            }
+            ListNode dummy = new ListNode(0);
+            dummy.next = head;
+            ListNode pre = dummy;
+
+            ListNode temp = head;
+            while (temp != null) {
+                if (temp.next != null && temp.val == temp.next.val) {
+                    while (temp.next != null && temp.val == temp.next.val) {
+                        temp = temp.next;
+                    }
+                    pre.next = temp.next;
+                } else {
+                    pre = pre.next;
+                }
+                temp = temp.next;
+            }
+            return dummy.next;
+        }
+    }
+}
