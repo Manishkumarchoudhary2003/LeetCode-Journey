8cf5bdb085e1b7a8c0773eb9579215e1d54c5c77
Manishkumarchoudhary2003 <cmanishkumar193@gmail.com>
2024-02-04 19:24:39 +0530

    04/02/2024

---
+++ xorOperations.java
@@ -0,0 +1,12 @@
+public class xorOperations {
+    class Solution {
+        public int xorOperation(int n, int start) {
+            int ans = start;
+            for (int i = 1; i < n; i++) {
+                ans ^= (start + 2 * i);
+            }
+            return ans;
+        }
+    }
+    
+}
